5a6eb484694483c139b6f0d97ec34217fcfc8d4b
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2022-09-26 16:22:37 +0200

    update dependencies

--- tests/test_equi7grid.py
+++ tests/test_equi7grid.py
@@ -13,7 +13,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """
-Tests for the Equi7Grid().
+Tests for the Equi7Grid.
 """
 
 import unittest
