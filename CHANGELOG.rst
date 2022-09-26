65e55f9354491fbc67f3fa8ef0f2fb40768791cd
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2022-09-26 14:14:51 +0200

    update project files

---
+++ CHANGELOG.rst
@@ -0,0 +1,83 @@
+=========
+Changelog
+=========
+
+Version 0.2.0
+=============
+
+- update project files using pyscaffold 4.3.1
+
+Version 0.1.0
+=============
+
+- support for stripped and tiled GeoTIFF blocks
+
+Version v0.0.14
+===============
+
+- minor updates in image2equi7
+- adds calc_length_distortion functions
+- adds create_distortion_files.py module
+
+Version v0.0.13
+===============
+
+- full update of all files _LAND.shp
+- revision of all .prj files
+- newly generated equi7grid.dat
+- full update of coverland lists within equi7grid.dat
+
+Version v0.0.10
+===============
+
+- in image2equi7()...
+- float64 removed
+- same outputfile as input possible
+- scale/offset is now accepted
+
+Version v0.0.9
+==============
+
+- bugfix in antimeridian handling
+- image2equi7 ingests now .nc with "bands"
+
+Version v0.0.8
+==============
+
+- extended search_tiles_in_roi()
+- fixes issues with projection strings when using GDAL>=3.0.0
+
+Version v0.0.7
+==============
+
+- fixed issues on antimeridian/dateline
+
+Version v0.0.6
+==============
+
+- minor update on lonlat2xy functions
+
+Version v0.0.5
+==============
+
+- minor update on tilesystem functions
+
+Version v0.0.4
+==============
+
+- python3_ready
+
+Version v0.0.3
+==============
+
+- Fix Python 3 issues
+
+Version v0.0.2
+==============
+
+- Second draft
+
+Version v0.0.1
+==============
+
+- First draft
---
+++ src/data/create_distortion_files.py
@@ -0,0 +1,121 @@
+# Copyright (c) 2022, TU Wien, Department of Geodesy and Geoinformation (GEO).
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of the FreeBSD Project.
+
+import numpy as np
+import os
+from datetime import datetime
+from veranda.io.geotiff import GeoTiffFile
+from equi7grid.equi7grid import Equi7Grid
+from geographiclib.geodesic import Geodesic
+from geopathfinder.naming_conventions.yeoda_naming import YeodaFilename
+
+
+def run(outpath, sampling):
+
+    # initialise objects
+    e7grid = Equi7Grid(sampling)
+    ellaxis = Geodesic.WGS84.a
+
+    # loop over the continents
+    for sg in e7grid.subgrids.keys():
+
+        subgrid = e7grid.subgrids[sg]
+
+        sg_str = sg + str(sampling).zfill(3) + 'M'
+        sg_path = os.path.join(outpath, 'EQUI7_' + sg_str)
+        if not os.path.exists(sg_path):
+            os.makedirs(sg_path)
+
+        # get the projected projection centre
+        fe = subgrid.core.projection.osr_spref.GetProjParm('false_easting')
+        fn = subgrid.core.projection.osr_spref.GetProjParm('false_northing')
+
+        # loop over the tiles that cover land
+        tiles = subgrid.tilesys.list_tiles_covering_land()
+        for t in tiles:
+
+            t_path = os.path.join(sg_path, t)
+            if not os.path.exists(t_path):
+                os.makedirs(t_path)
+
+            yfields = {
+                'var_name': 'K-DISTORTION',
+                'datetime_1': '',
+                'datetime_2': '',
+                'band': '',
+                'extra_field': '',
+                'tile_name': t,
+                'grid_name': sg_str,
+                'version_run_id': 'V01R01',
+                'sensor_field': 'EQUI7GRIDV14'
+            }
+            filename = YeodaFilename(yfields)
+
+            tile = subgrid.tilesys.create_tile(t)
+            gt = tile.geotransform()
+
+            # create all pixels in projected coordinates
+            xv, yv = np.meshgrid(
+                np.arange(gt[0] + gt[1] / 2, gt[0] + tile.core.tile_xsize_m,
+                          gt[1]),
+                np.arange(gt[3] + gt[5] / 2, gt[3] - tile.core.tile_ysize_m,
+                          gt[5]))
+
+            # create the distances to the projection centre
+            dists = np.sqrt((xv - fe)**2 + (yv - fn)**2)
+
+            # apply equation for distortion in direction perpendicular to the radius, k:
+            # k = c/geod.a / np.sin(c/geod.a)
+            #
+            # is it just about the distance to the centre (c), and as they should be equally long
+            # on the ellipsoid and on the projected plane (the core of of AEQD!), this should be fine.
+            ks = dists / ellaxis / np.sin(dists / ellaxis)
+
+            md = {}
+            md['creation_date'] = datetime.now()
+            md['scale_factor'] = 1.0 / 10000
+            md['add_offset'] = 0
+            md['nodata'] = -9999
+
+            with GeoTiffFile(os.path.join(t_path, filename._build_fn()),
+                             mode='w',
+                             count=1,
+                             gdal_opt={'COMPRESS': 'ZSTD'},
+                             geotransform=gt,
+                             spatialref=tile.core.projection.wkt) as gt_file:
+                gt_file.write(np.round(ks * 10000).astype(np.int16),
+                              band=1,
+                              scale_factor=md['scale_factor'],
+                              add_offset=md['add_offset'],
+                              nodata=md['nodata'])
+
+
+if __name__ == '__main__':
+    outpath = r'D:\Arbeit\atasks\202104_equi7grid_distortion_plots_2'
+    sampling = 250
+    run(outpath, sampling)
