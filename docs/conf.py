c0e7761f7c146c8bc3d0e0f9c8b8a78a2e887e6b
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2022-09-27 18:55:44 +0200

    update doc config

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "equi7grid"
-copyright = "2022, Sebastian Hahn"
+copyright = "2022, TU Wien"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -145,15 +145,15 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
-}
+# html_theme_options = {
+#     "sidebar_width": "300px",
+#     "page_width": "1200px"
+# }
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -243,7 +243,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "equi7grid Documentation", "Sebastian Hahn", "manual")
+    ("index", "user_guide.tex", "equi7grid Documentation", "TU Wien", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
