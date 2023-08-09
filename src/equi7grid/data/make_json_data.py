2692baa9b9acce1fe0293d8a4d4a1d92bab2d5e8
bbm <Bernhard.Bauer-Marschallinger@geo.tuwien.ac.at>
2023-08-09 15:51:16 +0200

    just a dummy commit for testing

--- src/equi7grid/data/make_json_data.py
+++ src/equi7grid/data/make_json_data.py
@@ -39,12 +39,134 @@ from geospade.raster import RegularMosaicGeometry
 from geospade.raster import Tile
 from geospade.crs import SpatialRef
 
+from osgeo import ogr
+
+class Equi7Tile(Tile):
+
+    def __init__(self, n_rows, n_cols, sref,
+                 geotrans=(0, 1, 0, 0, 0, -1),
+                 mosaic_topology="INNER",
+                 active=True,
+                 metadata=None,
+                 name=None,
+                 description="",
+                 px_origin="ul",
+                 parent=None):
+
+        super().__init__(n_rows, n_cols, sref,
+                         geotrans=geotrans,
+                         mosaic_topology=mosaic_topology,
+                         active=active,
+                         metadata=metadata,
+                         name=name,
+                         description=description,
+                         px_origin=px_origin,
+                         parent=parent)
+
+    def to_definition(self) -> dict:
+        tile_dict = super().to_definition()
+        del tile_dict['spatial_reference']
+        del tile_dict['pixel_origin']
+        del tile_dict['number_of_rows']
+        del tile_dict['number_of_columns']
+
+        return tile_dict
+
+class Equi7Subgrid(RegularMosaicGeometry):
+
+    def __init__(self, tiles, boundary=None, adjacency_matrix=None, name="",
+                 description="",
+                 check_consistency=True,
+                 **kwargs):
+
+        super().__init__(tiles, boundary=boundary, adjacency_matrix=adjacency_matrix, name=name,
+                         description=description,
+                         check_consistency=check_consistency, **kwargs)
+
+    def to_definition(self) -> dict:
+        subgrid_dict_temp = super().to_definition()
+        ref_tile = self.tiles[0]
+        subgrid_dict_temp['spatial_reference'] = ref_tile.sref.to_proj4_dict()
+        subgrid_dict_temp['spatial_reference_wkt'] = ref_tile.sref.wkt
+        subgrid_dict_temp['pixel_origin'] = ref_tile.px_origin
+        subgrid_dict_temp['number_of_rows'] = ref_tile.n_rows
+        subgrid_dict_temp['number_of_columns'] = ref_tile.n_cols
+
+        grid_header_atts = ['name', 'type', 'tile_class', 'number_of_rows', 'number_of_columns',
+                            'pixel_origin', 'description',
+                            'spatial_reference', 'spatial_reference_wkt']
+
+        subgrid_dict = {k: None for k in grid_header_atts}
+        subgrid_dict.update(subgrid_dict_temp)
+        return subgrid_dict
+
+    @classmethod
+    def from_definition(cls, definition, check_consistency=True) -> "Equi7Subgrid":
+        """
+        Creates a mosaic geometry from a human-readable mosaic definition, which is a
+        dictionary containing the following elements:
+            - 'type'
+            - 'boundary'
+            - 'name'
+            - 'description'
+            - 'adjacency_matrix'
+            - 'tile_class'
+            - 'tiles'
+
+        The expected values can be taken from the `MosaicGeometry`, `Tile`, and `RasterGeometry` constructor docs.
+
+        Parameters
+        ----------
+        definition : dict
+            Human-readable definition of a mosaic.
+        check_consistency : bool, optional
+            If True, the tiles are checked for consistency, i.e. to be non-overlapping (defaults to True).
+
+        Returns
+        -------
+        geospade.raster.MosaicGeometry
+
+        """
+        mosaic_type = definition['type']
+        if mosaic_type != cls._type:
+            err_msg = "Mosaic type of definition '{}' does not match expected mosaic type '{}'".format(mosaic_type, cls._type)
+            raise ValueError(err_msg)
+        mosaic_boundary = ogr.CreateGeometryFromWkt(definition['boundary'])
+        mosaic_name = definition['name']
+        description = definition['description']
+        adjacency_matrix = None if definition['adjacency_matrix'] is None else np.array(definition['adjacency_matrix'])
+
+        tiles = []
+        tile_class_name = definition['tile_class']
+        tile_class = globals().get(tile_class_name)
+        if tile_class is None:
+            err_msg = "Tile class '{}' must be imported.".format(tile_class_name)
+            raise ImportError(err_msg)
+
+        for key in definition['tiles'].keys():
+            tile_def = definition['tiles'][key]
+            tile_def['spatial_reference'] = definition['spatial_reference']
+            tile_def['pixel_origin'] = definition['pixel_origin']
+            tile_def['number_of_rows'] = definition['number_of_rows']
+            tile_def['number_of_columns'] = definition['number_of_columns']
+            tiles.append(tile_class.from_definition(tile_def))
+
+        mosaic_boundary.AssignSpatialReference(tiles[0].sref.osr_sref)
+        return cls.from_tile_list(tiles, boundary=mosaic_boundary, adjacency_matrix=adjacency_matrix, name=mosaic_name, description=description,
+                                  check_consistency=check_consistency)
+
+
+    def from_rectangular_definition(cls, my_keywords) -> "Equi7Subgrid":
+
+        pass
+
+        return cls.from_tile_list(tile_objects, name=mosaic_name, boundary=boundary)
 
 def run1():
 
-    eg = Equi7Grid(500)
+    eg = Equi7Grid(10)
 
-    for sg in ['EU']:#eg.subgrids:
+    for sg in ['AS']:#eg.subgrids:
 
         boundary = eg.subgrids[sg].polygon_proj
 
@@ -81,8 +203,8 @@ def run1():
             elif intersects:
                 mosaic_topo = 'BOUNDARY'
 
-            gst = Tile(n_rows, n_cols, sref, geotrans=geotransform, name=name, mosaic_topology=mosaic_topo,
-                       active=tile in at_sh, px_origin='ll',
+            gst = Equi7Tile(n_rows, n_cols, sref, geotrans=geotransform, name=name, mosaic_topology=mosaic_topo,
+                       active=tile in at_sh, px_origin='ul',
                        metadata=md)
 
             #gst.to_json(r"D:\Arbeit\atasks\202307_equi7grid_update\geojson\tests.json")
@@ -91,8 +213,8 @@ def run1():
         mosaic_name = "{} subgrid".format(sg)
 
 
-        mosaic_geom = RegularMosaicGeometry.from_tile_list(tile_objects, name=mosaic_name, boundary=boundary)
-        mosaic_geom.to_json(r"D:\Arbeit\atasks\202307_equi7grid_update\geojson\{}_5.json".format(sg))
+        mosaic_geom = Equi7Subgrid.from_tile_list(tile_objects, name=mosaic_name, boundary=boundary)
+        mosaic_geom.to_json(r"D:\Arbeit\atasks\202307_equi7grid_update\geojson\{}_13.json".format(sg))
         pass
 
 def run3():
@@ -120,7 +242,6 @@ def run3():
         mosaic_name = "{} subgrid".format(sg)
 
 
-
         mg = RegularMosaicGeometry.from_rectangular_definition(n_rows, n_cols, tilesize, tilesize, sref,
                                     geotrans=gt, tile_class=Tile, tile_kwargs=None,
                                     name_frmt="E{:03d}N{:03d}" + tilecode, boundary=bd, name=mosaic_name, description="")
@@ -142,6 +263,14 @@ def run2():
     #plt.show()
     mosaicgeom.plot(label_tiles=True, show=True)
     RegularMosaicGeometry.from_rectangular_definition()
+    pass
+
+def run4():
+
+    a = Equi7Subgrid.from_json(r"D:\Arbeit\atasks\202307_equi7grid_update\geojson\EU_13.json")
+
+    a.plot(show=True, label_tiles=True)
+
     pass
 
 if __name__ == '__main__':
