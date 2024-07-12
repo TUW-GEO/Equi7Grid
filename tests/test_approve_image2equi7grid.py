4b4805a420176a1e7630ff251d3ac0046a22c821
Tobias Stachl <tobias.stachl@eodc.eu>
2024-07-12 12:16:27 +0200

    add simple test

--- tests/test_approve_image2equi7grid.py
+++ tests/test_approve_image2equi7grid.py
@@ -5,7 +5,7 @@ from approvaltests.approvals import verify_file
 from approvaltests.namer import NamerFactory
 
 from equi7grid.equi7grid import Equi7Grid
-from equi7grid.image2equi7grid import image2equi7grid
+from equi7grid.image2equi7grid import TileResampler, image2equi7grid
 
 
 @pytest.fixture
@@ -27,3 +27,17 @@ def test_approve_imag2equi7grid(input_dir, out_dir):
                 options=NamerFactory.with_parameters("E018N066T6"))
     verify_file((out_dir / "EQUI7_EU100M/E072N030T6/lake_in_russia_lonlat_EU100M_E072N030T6.tif").as_posix(),
                 options=NamerFactory.with_parameters("E072N030T6"))
+
+
+@pytest.mark.skipif(os.name == 'nt', reason="CI Windows has troubles creating directories")
+def test_multiprocessing(input_dir, out_dir):
+    # begin-snippet: image2equi7grid-example
+    input_file = input_dir / "lake_in_russia_lonlat.tif"
+
+    tile_resampler = TileResampler(Equi7Grid(10), input_file.as_posix(), out_dir.as_posix(), parallelism=12)
+    results = tile_resampler.resample_tiles()
+
+    print(results)
+
+    assert (out_dir / "EQUI7_AS010M/E021N069T1/lake_in_russia_lonlat_AS010M_E021N069T1.tif").exists()
+    assert (out_dir / "EQUI7_EU010M/E073N032T1/lake_in_russia_lonlat_EU010M_E073N032T1.tif").exists()
