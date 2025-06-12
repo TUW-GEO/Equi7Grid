6cf9386da7f8389186483ebddf1e81838190ece2
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2025-06-12 10:32:16 +0200

    code formatting, enclose testing code

--- src/equi7grid/equi7_to_lonlat.py
+++ src/equi7grid/equi7_to_lonlat.py
@@ -21,22 +21,22 @@ def chunk_calc(method, nodata, input_files):
     nodata : int/float
         nodata mask value will be ignored for calculations.
     input_files : list
-        list of files to be used as input 
+        list of files to be used as input
         (Must have identical extent/geotransform/proj etc., but no check is done)
     """
-    
+
     datasets = [gdal.Open(input_files[0], gdal.GA_Update)] + [gdal.Open(path) for path in input_files[1:]]
     bands = [ds.GetRasterBand(1) for ds in datasets]
-    
+
     x_size, y_size = datasets[0].RasterXSize, datasets[0].RasterYSize
     block_size_x, block_size_y = bands[0].GetBlockSize()
-    
+
     for y in range(0, y_size, block_size_y):
-        rows = min(block_size_y, y_size - y) 
-        
+        rows = min(block_size_y, y_size - y)
+
         for x in range(0, x_size, block_size_x):
-            cols = min(block_size_x, x_size - x) 
-            
+            cols = min(block_size_x, x_size - x)
+
             arrays = [band.ReadAsArray(x, y, cols, rows) for band in bands]
             stack = np.stack(arrays)
             if nodata is not None:
@@ -48,7 +48,7 @@ def chunk_calc(method, nodata, input_files):
             else:
                 stack_result = stack.mean(axis=0)
             bands[0].WriteArray(stack_result, x, y)
-            
+
     for band in bands:
         band.FlushCache()
         band = None
@@ -77,7 +77,7 @@ def equi7_to_lonlat(roi,
                     bigtiff=True,
                     ):
     """
-    Function to generate a GeoTIFF-file in lonlat-projection(epsg:4326) with a 
+    Function to generate a GeoTIFF-file in lonlat-projection(epsg:4326) with a
     user-defined extent and resolution from tiled Equi7Grid files.
 
     Parameters
@@ -105,14 +105,14 @@ def equi7_to_lonlat(roi,
         value flagged as nodata in the output image
     aggregation_method : str, optional
         Incase roi overlaps with several Equi7-subgrids, this parameter decides
-        how mutliple values per geographic location are handled. 
+        how mutliple values per geographic location are handled.
         minimum: 'min', maximum: 'max'(default), or mean: 'mean' are supported
     num_threads : int, optional
         number of threads used for gdal warping.
     resampling_type : str, optional
         gdal resampling method: bilinear, cubic, near (default), ...
     data_type : str, optional
-        force output image to have a specific datatype. Byte, UInt16, Int16, 
+        force output image to have a specific datatype. Byte, UInt16, Int16,
         UInt32, Int32, Float32, Float64, CInt16, CInt32, CFloat32 or CFloat64.
     compress : bool, optional
         Decision if output file is compressed. Defaults to True
@@ -124,7 +124,7 @@ def equi7_to_lonlat(roi,
         internal file blocksize of output file. The default is 512.
     bigtiff : bool, optional
         Decision if outputfile is a bigtiff format. Defaults to True
-        
+
     Returns
     -------
     True, if successfull.
@@ -133,20 +133,20 @@ def equi7_to_lonlat(roi,
     #dateline check
     if roi[0] > roi[2]:
         raise ValueError('Given bbox crosses antimeridian!')
-    
+
     #initialise the grid
     e7grid = Equi7Grid(pixelsize)
     #find intersecting tiles with region of interest
     tiles = e7grid.search_tiles_in_roi(bbox=[(roi[0], roi[1]), (roi[2], roi[3])])
     if not tiles:
         raise ValueError(f"No intersecting tiles found for region of interest {roi}.")
-    
+
     input_filepaths = OrderedDict()
     #get the file for each intersecting tile if it exists and save it grouped per subgrid (=continent-wise)
     for tile in tiles:
         sg = tile.split('_')[0]
         t = tile.split('_')[1]
-        
+
         #replace wildcards by actual values
         input_path = os.path.join(input_folder_path, input_file_path)
         input_path = input_path.replace('SUBGRID', sg)
@@ -154,21 +154,21 @@ def equi7_to_lonlat(roi,
 
         #find file matching the pattern
         file_path = glob.glob(input_path)
-        
+
         #if a file was found:
         if file_path:
             if len(file_path)>1:
                 print(f'Multiple files found for search path: {input_path}. Only first will be used.')
                 file_path = sorted(file_path)[0]
-            
+
             #create new dict entry incase not yet existing for sg
             if sg not in input_filepaths:
                 input_filepaths[sg] = []
-            
+
             #put filepath into correct list
             input_filepaths[sg].extend(file_path)
         else: print(f'No file found for search path: {input_path}')
-    
+
     #if not a single input file was found
     if not input_filepaths:
         raise FileNotFoundError("No input files were found for any intersecting tile. Aborting script.")
@@ -178,30 +178,30 @@ def equi7_to_lonlat(roi,
 
     #incase there are only files of one subgrid, no zonal overlap hast to be dealt with --> warping result of first subgrid will already be the final result
     onezone_flag = len(sorted_input_filepaths) == 1
-    
-    
+
+
     #loop through subgrids
     warping_results = []
     for sg, filepaths in sorted_input_filepaths:
         #build vrt for all files of a subgrid (no complex mosaicing needed, since tiles are co-registered and non-overlapping)
         vrt_path = full_output_path.replace('.tif', f'_{sg}_mosaic.vrt')
         vrt_options = {'xRes': "{}".format(e7grid.core.sampling),
-                       'yRes': "{}".format(e7grid.core.sampling)} 
-        vrt = gdal.BuildVRT(vrt_path, 
-                            filepaths, 
+                       'yRes': "{}".format(e7grid.core.sampling)}
+        vrt = gdal.BuildVRT(vrt_path,
+                            filepaths,
                             options=gdal.BuildVRTOptions(**vrt_options)
                             )
         vrt=None
-        
+
         # warp the vrt to the target extent/crs
         options = {
-            '-t_srs': 'EPSG:4326', 
+            '-t_srs': 'EPSG:4326',
             '-of': 'GTiff',
             '-te': " ".join(map(str, roi)),
             '-r': resampling_type,
             '-multi': ""
             }
-        
+
         options["-co"] = []
         if compress:
             options["-co"].append("COMPRESS={0}".format(compress_type))
@@ -215,7 +215,7 @@ def equi7_to_lonlat(roi,
             options["-co"].append("TILED=YES")
             options["-co"].append("BLOCKXSIZE={0}".format(blocksize))
             options["-co"].append("BLOCKYSIZE={0}".format(blocksize))
-        else:  
+        else:
             blockxsize = e7grid.core.tile_xsize_m // e7grid.core.sampling
             blockysize = blocksize
             options["-co"].append("TILED=NO")
@@ -225,13 +225,13 @@ def equi7_to_lonlat(roi,
             options["-co"].append("BIGTIFF=YES")
         if num_threads!= None:
             options["-wo"] = "\"NUM_THREADS={}\"".format(num_threads)
-            
-        
+
+
         #grab geotransform from first subgrid iteration if several subgrids involved
-        if warping_results:    
+        if warping_results:
             ds = open_image(warping_results[0])
             geotransform = ds.geotransform()
-            output_bounds = [geotransform[0], 
+            output_bounds = [geotransform[0],
                              geotransform[3]+geotransform[5]*ds.YSize(),
                              geotransform[0]+geotransform[1]*ds.XSize(),
                              geotransform[3]]
@@ -243,66 +243,58 @@ def equi7_to_lonlat(roi,
         #if there was no iteration before, but given px, then use it
         elif pixelsize_deg!=None:
             options['-tr'] =  "{} -{}".format(pixelsize_deg, pixelsize_deg)
-            
+
         # if only 1 subgrid, warping result is already final file
-        if onezone_flag: 
+        if onezone_flag:
             warp_path = full_output_path
         else: warp_path = full_output_path.replace('.tif', f'_{sg}.tif')
-        
+
         succeed, _ = call_gdal_util('gdalwarp',
                                     src_files=vrt_path,
                                     dst_file=warp_path,
                                     gdal_path=gdal_path,
                                     options=options)
-        
+
         #remove the temporary vrt file
         os.remove(vrt_path)
-        
+
         #if only 1 subgrid -- done!
         if onezone_flag:
             return True
         #else also warp other subgrids and merge afterwards
         else:
             warping_results.append(warp_path)
-     
+
     #Perform chunkwise file aggregation if data is fetched from several Equi7 subgrids
     chunk_calc(aggregation_method, output_nodata, warping_results)
     #Rename first file since aggregation result overwrite first input file
     os.rename(warping_results[0], full_output_path)
-    
+
     #remove temp files
     for wr in warping_results[1:]:
         os.remove(wr)
-    
+
     return True
 
-
-
-print('Running Test')
-# TEST: like test1, but with data on R: --> windows test suitable
-#       byte esa-worldcover dataset
-equi7_to_lonlat(roi=(9, 46, 18, 50),
-                pixelsize=20,
-                input_folder_path = r'R:\Datapool\ESA_WorldCover\02_processed\2021_v200\EQUI7_SUBGRID\TILE',
-                input_file_path = 'ESA-WorldCover-2021-V200_SUBGRID_TILE.tif',
-                full_output_path = r'D:\Arbeit\ctemp\EQUI7_to_LATLON2\WorldCover_46-50LON_9-18LAT_bbm.tif',
-                gdal_path = r'C:\Python\Miniconda310_22.11.1\envs\equi7grid\Library\bin',
-                pixelsize_deg=None,
-                input_nodata = 0,
-                output_nodata = 0,
-                aggregation_method=None,
-                resampling_type='near',
-                data_type=None,
-                compress=True,
-                compress_type='LZW',
-                blocksize=512,
-                bigtiff=True,
-                num_threads=1)
-print('Test done!')
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    print('Running Test')
+    # TEST: like test1, but with data on R: --> windows test suitable
+    #       byte esa-worldcover dataset
+    equi7_to_lonlat(roi=(9, 46, 18, 50),
+                    pixelsize=20,
+                    input_folder_path = r'R:\Datapool\ESA_WorldCover\02_processed\2021_v200\EQUI7_SUBGRID\TILE',
+                    input_file_path = 'ESA-WorldCover-2021-V200_SUBGRID_TILE.tif',
+                    full_output_path = r'D:\Arbeit\ctemp\EQUI7_to_LATLON2\WorldCover_46-50LON_9-18LAT_bbm.tif',
+                    gdal_path = r'C:\Python\Miniconda310_22.11.1\envs\equi7grid\Library\bin',
+                    pixelsize_deg=None,
+                    input_nodata = 0,
+                    output_nodata = 0,
+                    aggregation_method=None,
+                    resampling_type='near',
+                    data_type=None,
+                    compress=True,
+                    compress_type='LZW',
+                    blocksize=512,
+                    bigtiff=True,
+                    num_threads=1)
+    print('Test done!')
